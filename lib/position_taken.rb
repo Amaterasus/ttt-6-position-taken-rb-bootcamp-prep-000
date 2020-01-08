c8cb5508d1f96f1975a30834204ebea35dbe8391
Skye Geraghty <skyetay03@googlemail.com>
2020-01-08 23:00:09 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,5 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, pos)
-  return board[pos] != " " || board[pos] != "" ? false : true
+  return board[pos] != " " || board[pos] != "" ? false : board[pos] == "X" || board[pos] == "O" ? true : nil
 end
